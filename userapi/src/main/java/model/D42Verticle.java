661955f38f916ac64637875eae84a65d61d80f3e
Rohit Patiyal <rohit.patiyal@flipkart.com>
2017-01-18 20:51:26 +0530

    Added Vert.x web verticle for userapi

---
+++ userapi/src/main/java/model/D42Verticle.java
@@ -0,0 +1,10 @@
+package model;
+
+import io.vertx.core.AbstractVerticle;
+
+/**
+ * Connects with the D42 Simple Storage Service
+ * Created by rohit.patiyal on 18/01/17.
+ */
+public class D42Verticle extends AbstractVerticle {
+}
