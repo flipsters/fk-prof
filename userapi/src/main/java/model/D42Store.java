a324510b72ca7618b00f746ef1589dc6aa0361aa
Rohit Patiyal <rohit.patiyal@flipkart.com>
2017-01-23 18:05:05 +0530

    Implemented get Profile API ; Added Interface for Datasource

--- userapi/src/main/java/model/D42Store.java
+++ userapi/src/main/java/model/D42Store.java
@@ -11,16 +11,16 @@ import com.amazonaws.services.s3.model.ObjectListing;
 import com.amazonaws.services.s3.model.S3ObjectSummary;
 import org.apache.commons.codec.binary.Base32;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeParseException;
+import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * Connects with the D42 Simple Storage Service
  * Created by rohit.patiyal on 19/01/17.
  */
-public class D42Store {
+public class D42Store implements IDataStore {
     private static final String ACCESS_KEY = "66ZX9WC7ZRO6S5BSO8TG";
     private static final String SECRET_KEY = "fGEJrdiSWNJlsZTiIiTPpUntDm0wCRV4tYbwu2M+";
     private static final String S3_BACKUP_ELB_END_POINT = "http://10.47.2.3:80";
@@ -31,41 +31,34 @@ public class D42Store {
     private Base32 base32 = null;
 
     public D42Store() {
+        initDataStore();
+        base32 = new Base32();
+    }
+
+    private static String getWorkType(String key) {
+        String[] splits = key.split("_");
+        return splits[6];
+
+    }
+
+    private static Profile getProfile(String key) {
+        String[] splits = key.split("_");
+        String start = splits[4];
+        return new Profile(start, ZonedDateTime.parse(start).plusSeconds(Long.parseLong(splits[5])).toString());
+    }
+
+    public void initDataStore() {
         conn = new AmazonS3Client(
                 new BasicAWSCredentials(ACCESS_KEY, SECRET_KEY),
                 new ClientConfiguration().withProtocol(Protocol.HTTP));
         conn.setEndpoint(S3_BACKUP_ELB_END_POINT);
         conn.setS3ClientOptions(new S3ClientOptions().withPathStyleAccess(true));
         conn.getS3AccountOwner().getId();
-        base32 = new Base32();
-    }
-
-    private String getAppIdFromKey(String key) {
-        return key.split("_")[1];
     }
 
     private String getLastFromCommonPrefix(String commonPrefix) {
         String[] splits = commonPrefix.split("_");
         return splits[splits.length - 1];
-    }
-
-    private String getClusterIdFromKey(String key) {
-        return key.split("_")[2];
-    }
-
-    private List<String> getObjectNameWithPrefix(String prefix) {
-        List<String> objectNames = new ArrayList<>();
-        ObjectListing objects = conn.listObjects(new ListObjectsRequest()
-                .withBucketName(BUCKET_NAME).withDelimiter(DELIMITER).withPrefix(prefix));
-        do {
-            for (S3ObjectSummary objectSummary : objects.getObjectSummaries()) {
-                System.out.println(objectSummary.getSize());
-                objectNames.add(objectSummary.getKey());
-
-            }
-            objects = conn.listNextBatchOfObjects(objects);
-        } while (objects.isTruncated());
-        return objectNames;
     }
 
     private List<String> getCommonPrefixes(String prefix) {
@@ -80,49 +73,74 @@ public class D42Store {
         return commonPrefixes;
     }
 
-    public Set<String> getAppIdsWithPrefix(String appIdPrefix) {
-        String prefix = VERSION + DELIMITER;
-        List<String> commonPrefixes = getCommonPrefixes(prefix);
-        Set<String> appIds = new HashSet<>();
+    private Set<String> getAllWithPrefix(String prefix) {
+        Set<String> allObjects = new HashSet<>();
+        ObjectListing objects = conn.listObjects(new ListObjectsRequest()
+                .withBucketName(BUCKET_NAME).withPrefix(prefix));
+        do {
+            for (S3ObjectSummary objSummary : objects.getObjectSummaries()) {
+                allObjects.add(objSummary.getKey());
+                System.out.println(objSummary.getKey());
+            }
+        } while (objects.isTruncated());
+        return allObjects;
+    }
+
+    private Set<String> getListingAtLevelWithPrefix(String level, String objPrefix, boolean encoded) {
+        List<String> commonPrefixes = getCommonPrefixes(level);
+        Set<String> objs = new HashSet<>();
         for (String commonPrefix : commonPrefixes) {
-            String decodedAppId = new String(base32.decode(getLastFromCommonPrefix(commonPrefix).getBytes()));
-            if (decodedAppId.indexOf(appIdPrefix) == 0) {
-                appIds.add(decodedAppId);
+            String objName = getLastFromCommonPrefix(commonPrefix);
+            if (encoded) {
+                objName = new String(base32.decode(objName.getBytes()));
+            }
+            if (objName.indexOf(objPrefix) == 0) {
+                objs.add(objName);
             }
         }
-        return appIds;
+        return objs;
     }
 
+    @Override
+    public Set<String> getAppIdsWithPrefix(String appIdPrefix) {
+        return getListingAtLevelWithPrefix(VERSION + DELIMITER, appIdPrefix, true);
 
+    }
+
+    @Override
     public Set<String> getClusterIdsWithPrefix(String appId, String clusterIdPrefix) {
-        String prefix = VERSION + DELIMITER + new String(base32.encode(appId.getBytes())) + DELIMITER;
-        List<String> objectNames = getObjectNameWithPrefix(prefix);
-        Set<String> clusterIds = new HashSet<>();
-        System.err.println(objectNames.size() + "OBJ SIZE ");
-        for (String objName : objectNames) {
-            System.err.println(objName);
-            String decodedClusterId = new String(base32.decode(getClusterIdFromKey(objName).getBytes()));
-            if (decodedClusterId.indexOf(clusterIdPrefix) == 0) {
-                clusterIds.add(decodedClusterId);
-            }
-        }
-        return clusterIds;
+        return getListingAtLevelWithPrefix(
+                VERSION + DELIMITER + new String(base32.encode(appId.getBytes())) + DELIMITER,
+                clusterIdPrefix,
+                true);
     }
 
+    @Override
     public Set<String> getProcIdsWithPrefix(String appId, String clusterId, String procIdPrefix) {
-        String prefix = VERSION + DELIMITER + new String(base32.encode(appId.getBytes()))
-                + DELIMITER + new String(base32.encode(clusterId.getBytes()));
-        List<String> objectNames = getObjectNameWithPrefix(prefix);
-        Set<String> clusterIds = new HashSet<>();
-
-        for (String objName : objectNames) {
-//            String decodedProcId = new String(base32.decode(.getBytes()));
-//            if(getProcIdFromKey(objName).indexOf(procIdPrefix) == 0){
-//                clusterIds.add(decodedClusterId);
-//            }
-        }
-        return clusterIds;
+        return getListingAtLevelWithPrefix(VERSION + DELIMITER + new String(base32.encode(appId.getBytes()))
+                        + DELIMITER + new String(base32.encode(clusterId.getBytes())) + DELIMITER,
+                procIdPrefix,
+                false);
     }
 
+    @Override
+    public Set<Profile> getProfilesInTimeWindow(String appId, String clusterId, String proc, String startTime, String durationInSeconds) {
+        String prefix = VERSION + DELIMITER + new String(base32.encode(appId.getBytes()))
+                + DELIMITER + new String(base32.encode(clusterId.getBytes())) + DELIMITER + startTime;
+        Map<Profile, Set<String>> profilesMap = getAllWithPrefix(prefix).stream().collect(Collectors.groupingBy(D42Store::getProfile, Collectors.mapping(D42Store::getWorkType, Collectors.toSet())));
+        Set<Profile> profiles = profilesMap.entrySet().stream().map(profileSetEntry -> {
+            profileSetEntry.getKey().setValues(profileSetEntry.getValue());
+            return profileSetEntry.getKey();
+        }).collect(Collectors.toSet());
+        try {
+            ZonedDateTime startTimeAsTime = ZonedDateTime.parse(startTime);
+            Long durationInSecondsAsLong = Long.parseLong(durationInSeconds);
+            profiles = profiles.stream().filter(k -> ZonedDateTime.parse(k.getEnd()).isAfter(startTimeAsTime.plusSeconds(durationInSecondsAsLong))).collect(Collectors.toSet());
+        } catch (DateTimeParseException e) {
+            System.err.println(e);
+        }
+
+        return profiles;
+    }
 
 }
