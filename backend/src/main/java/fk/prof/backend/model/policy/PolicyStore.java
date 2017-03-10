648bbe2c6df6d6d570aa695a88d843b86b01c9c9
Rohit Patiyal <rohit.patiyal@flipkart.com>
2017-03-10 19:09:28 +0530

    Implemented cache based ZKStore for Policy (proto) with tests

---
+++ backend/src/main/java/fk/prof/backend/model/policy/PolicyStore.java
@@ -0,0 +1,23 @@
+package fk.prof.backend.model.policy;
+
+import policy.PolicyDetails;
+import recording.Recorder;
+
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+
+/**
+ * Interface for accessing the store containing policy information
+ * Created by rohit.patiyal on 07/03/17.
+ */
+public interface PolicyStore {
+  PolicyDetails getAssociatedPolicy(Recorder.ProcessGroup processGroup);
+
+  Map<Recorder.ProcessGroup, PolicyDetails> getAssociatedPolicies(String appId);
+
+  Map<Recorder.ProcessGroup, PolicyDetails> getAssociatedPolicies(String appId, String clusterId);
+
+  Map<Recorder.ProcessGroup, PolicyDetails> getAssociatedPolicies(String appId, String clusterId, String process);
+
+  CompletableFuture<Void> setPolicy(Recorder.ProcessGroup processGroup, PolicyDetails policyDetails);
+}
