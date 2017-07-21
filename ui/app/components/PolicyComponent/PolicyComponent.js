ccb96dc99738ae7f2827ca1d1b8f1130e1e643c1
Rohit Patiyal <rohit.patiyal@flipkart.com>
2017-07-21 18:54:07 +0530

    Added state in PolicyComponent and connected WorkType inputs to the parent state

--- ui/app/components/PolicyComponent/PolicyComponent.js
+++ ui/app/components/PolicyComponent/PolicyComponent.js
@@ -5,85 +5,158 @@ import Loader from '../LoaderComponent/LoaderComponent';
 export default class PolicyComponent extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {status: "GET_INIT", policy: {duration}, duration:""};
-    this.policyGetStatusResultText = this.policyGetStatusResultText.bind(this);
-    this.handleChange = this.handleChange.bind(this);
-
+    this.handleFormChange = this.handleFormChange.bind(this);
+    this.handleWorkTypeChangeInForm = this.handleWorkTypeChangeInForm.bind(this);
+    this.state = {
+      query: {
+        type: "GET",
+        resp: null,
+      },
+      alert : {
+        msg: "",
+        instr: ""
+      },
+      form: {
+        state: "LOADING",
+        values: {
+          duration: "",
+          coverage_pct: "",
+          description: "",
+          work: [],
+        }
+      }
+    };
   }
 
   componentDidMount() {
     console.log("Making a GET request");
-    this.setState({status: "GET_PENDING"});
+    // this.setState({
+    //   status: "GET_PENDING"
+    // });
     // window.setTimeout(() => this.setState({
     //   status: "GET_SUCCESS",
     //   policy: {duration: 120, coverage: 10, work: {work_type: 1}}
     // }), 2000);
-    window.setTimeout(() => this.setState({status: "GET_SUCCESS"}), 2000);
+    window.setTimeout(() => this.setState({
+      query: {
+        type: "GET",
+        resp: "200",
+      },
+      alert: {
+        msg: "Policy Found",
+        instr: "Please find the details below"
+      },
+      form: {
+        state: "UPDATE",
+        values: {
+          duration: "240",
+          coverage_pct: "10",
+          description: "some service",
+          work: [{
+            w_type: "cpu_sample",
+            cpu_sample: {
+              frequency: "50",
+              max_frames: "64"
+            }
+          }],
+        }
+      }
+    }), 2000);
   }
 
-  componentDidUpdate () {
+  componentDidUpdate() {
     console.log("Component Did Update");
     componentHandler.upgradeDom(); // eslint-disable-line
   }
 
-  componentWillReceiveProps(newProps) {
-    if (newProps.proc !== this.props.proc) {
-      console.log("Making a GET request");
-      this.setState({status: "GET_PENDING"});
-      // window.setTimeout(() => this.setState({
-      //   status: "GET_SUCCESS",
-      //   policy: {duration: 120, coverage: 10, work: {work_type: 1}}
-      // }), 2000);
-      window.setTimeout(() => this.setState({status: "GET_SUCCESS"}), 2000);
-    }
-  }
 
-  policyGetStatusResultText() {
-    if (this.state.status === "GET_SUCCESS") {
-      if (this.state.policy !== null) {
-        return (<div className="mdl-grid">
-          <div className="mdl-cell mdl-cell--11-col mdl-layout--middle">
-            <h2 className="mdl-card__title-text">Policy found</h2>Please find details below
-          </div>
-          <div className="mdl-layout-spacer"/>
-          <div className="mdl-cell mdl-cell--1-col mdl-layout--middle">
-            <button
-              className="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent"
-              style={{background: 'rgb(137, 137, 132)', margin: '0 auto'}}>Update
-            </button>
-          </div>
-        </div>)
-      } else {
-        return (<div className="mdl-grid">
-          <div className="mdl-cell mdl-cell--11-col mdl-layout--middle">
-            <h2 className="mdl-card__title-text">No policy found</h2>Create a new one by adding details below
-          </div>
-          <div className="mdl-layout-spacer"/>
-          <div className="mdl-cell mdl-cell--1-col mdl-layout--middle">
-            <button
-              className="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent"
-              style={{background: 'rgb(137, 137, 132)', margin: '0 auto'}}>Create
-            </button>
-          </div>
-        </div>)
-      }
-    }
-  }
+  // componentWillReceiveProps(newProps) {
+  //   if (newProps.proc !== this.props.proc) {
+  //     console.log("Making a GET request");
+  //     this.setState({status: "GET_PENDING"});
+  //     // window.setTimeout(() => this.setState({
+  //     //   status: "GET_SUCCESS",
+  //     //   policy: {duration: 120, coverage: 10, work: {work_type: 1}}
+  //     // }), 2000);
+  //     window.setTimeout(() => this.setState({status: "GET_SUCCESS"}), 2000);
+  //   }
+  // }
 
-  handleChange(e){
+  handleFormChange(e) {
     const target = e.target;
     const value = target.value;
     const id = target.id;
-    console.log(id + "  " + value);
-    this.setState({
-        [id]: value
-    });
+    this.setState((prevState) => ({
+        form: {
+          state: prevState.form.state,
+          values: {
+            ...prevState.form.values,
+            [id]: value
+          }
+        }
+      })
+    );
   }
 
+  handleWorkTypeChangeInForm(e) {
+    const prevWorks = this.state.form.values.work;
+
+    const target = e.target;
+    const value = target.value;
+
+    const w_type = target.dataset.wType;
+    const attribute = target.dataset.attribute;
+
+    const works = prevWorks.filter((work) => {
+      return work.w_type === w_type;
+    });
+    let w = {};
+    if (works.length === 0) {
+      w = {
+        w_type,
+        [w_type]: {
+          [attribute]: value
+        }
+      }
+    } else {
+      //works array must be of size one in this block
+      w = {
+        w_type,
+        [w_type]: {...works[0][w_type], [attribute]: value}
+      }
+    }
+    if (Object.values(w[w_type]).every((el) => (el === ""))) { //Remove work type element from work array if all of the attributes are empty
+      this.setState((prevState) => ({
+          form: {
+            state: prevState.form.state,
+            values: {
+              ...prevState.form.values,
+              work: [...prevState.form.values.work.filter((w) => {
+                return w.w_type !== w_type
+              })]
+            }
+          }
+        })
+      );
+    } else {
+      this.setState((prevState) => ({
+          form: {
+            state: prevState.form.state,
+            values: {
+              ...prevState.form.values,
+              work: [...prevState.form.values.work.filter((w) => {
+                return w.w_type !== w_type
+              }), w]
+            }
+          }
+        })
+      );
+    }
+  }
 
   render() {
-    if (!this.state.status) return null;
-    if (this.state.status === 'GET_PENDING') {
+    if (!this.state.query) return null;
+    if (this.state.query.type === 'GET' && this.state.query.resp === null) {
       return (
         <div>
           <h4 style={{textAlign: 'center'}}>Please wait, coming right up!</h4>
@@ -91,41 +164,63 @@ export default class PolicyComponent extends React.Component {
         </div>
       );
     }
-    if (this.props.status === 'GET_ERROR') {
+    if (this.state.query.type === 'GET' && this.state.query.resp === "500") {
       return (
         <div className={styles.card}>
           <h2>Failed to fetch the data. Please refresh or try again later</h2>
         </div>
       );
     }
+
+    const workArray = this.state.form.values.work;
+
+    const cpu_sample_work = workArray.some((w) => w.w_type === "cpu_sample")? workArray.filter((w) => w.w_type === "cpu_sample")[0].cpu_sample : " ";
+    const thread_sample_work = workArray.some((w) => w.w_type === "thread_sample")? workArray.filter((w) => w.w_type === "thread_sample")[0].thread_sample : " ";
+    const monitor_block_work = workArray.some((w) => w.w_type === "monitor_block")? workArray.filter((w) => w.w_type === "monitor_block")[0].monitor_block : " ";
+    const monitor_wait_work = workArray.some((w) => w.w_type === "monitor_wait")? workArray.filter((w) => w.w_type === "monitor_wait")[0].monitor_wait : " ";
     return (
       <div className="mdl-cell mdl-cell--11-col  mdl-shadow--3dp">
         <div className="mdl-grid mdl-grid--no-spacing">
           <div className="mdl-cell mdl-cell--12-col mdl-shadow--2dp">
-            {this.policyGetStatusResultText()}
+            <div className="mdl-grid">
+              <div className="mdl-layout-spacer"/>
+              <div className="mdl-cell mdl-cell--9-col mdl-layout--middle">
+                <h2 className="mdl-card__title-text">{this.state.alert.msg}</h2>{this.state.alert.instr}
+              </div>
+              <div className="mdl-layout-spacer"/>
+              <div className="mdl-cell mdl-cell--2-col mdl-layout--middle">
+                <button
+                  className="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent"
+                  style={{background: 'rgb(137, 137, 132)'}}>{this.state.form.state === "UPDATE"? "UPDATE": "CREATE"}
+                </button>
+              </div>
+            </div>
           </div>
           <div className="mdl-cell mdl-cell--12-col mdl-shadow--2dp">
             <div className="mdl-grid">
               <div className="mdl-cell mdl-cell--4-col mdl-cell--middle">
                 <div>Duration (in secs)</div>
                 <div className="mdl-textfield mdl-js-textfield">
-                  <input className="mdl-textfield__input" type="text" pattern="^[1-9][0-9][0-9]$" id="duration" onChange={this.handleChange} value={this.state.duration}/>
+                  <input className="mdl-textfield__input" type="number" min="100" max="960" id="duration"
+                         onChange={this.handleFormChange} value={this.state.form.values.duration}/>
                   <label className="mdl-textfield__label" htmlFor="duration">120</label>
-                  <span className="mdl-textfield__error">Input should be between 100-960!</span>
+                  <span className="mdl-textfield__error">Input should be between 100-960</span>
                 </div>
               </div>
               <div className="mdl-cell mdl-cell--4-col mdl-cell--middle">
                 <div>Coverage Percentage</div>
                 <div className="mdl-textfield mdl-js-textfield">
-                  <input className="mdl-textfield__input" type="text" pattern="^[0-9][0-9]?$|^100$" id="coverage_pct" onChange={this.handleChange} value={this.state.policy? this.state.policy.coverage_pct:""}/>
+                  <input className="mdl-textfield__input" type="number" min="0" max="100" id="coverage_pct"
+                         onChange={this.handleFormChange} value={this.state.form.values.coverage_pct}/>
                   <label className="mdl-textfield__label" htmlFor="coverage_pct">10</label>
-                  <span className="mdl-textfield__error">Input should be between 0-100!</span>
+                  <span className="mdl-textfield__error">Input should be between 0-100</span>
                 </div>
               </div>
               <div className="mdl-cell mdl-cell--4-col mdl-cell--middle">
                 <div>Description</div>
                 <div className="mdl-textfield mdl-js-textfield">
-                <textarea className="mdl-textfield__input" type="text" rows="3" id="description" onChange={this.handleChange} value={this.state.policy? this.state.policy.description:""}>
+              <textarea className="mdl-textfield__input" type="text" rows="3" id="description"
+                        onChange={this.handleFormChange} value={this.state.form.values.description}>
                 </textarea>
                   <label className="mdl-textfield__label" htmlFor="description">#servicename #nfrpolicy ...</label>
                 </div>
@@ -134,10 +229,18 @@ export default class PolicyComponent extends React.Component {
           </div>
           <div className="mdl-cell mdl-cell--12-col">
             <div className="mdl-grid mdl-grid--no-spacing">
-              <WorkType name="CPUSampling" attributes={["Frequency", "Max Frames"]} isDisabled={false}/>
-              <WorkType name="Thread Sampling" attributes={["Frequency", "Max Frames"]} isDisabled={true}/>
-              <WorkType name="Monitor Contention" attributes={["Max Monitors", "Max Frames"]} isDisabled={true}/>
-              <WorkType name="Monitor Wait" attributes={["Max Monitors", "Max Frames"]} isDisabled={true}/>
+              <WorkType name="CPU Sampling" attributes={["Frequency", "Max Frames"]} isDisabled={false}
+                        w_type="cpu_sample" onChange={this.handleWorkTypeChangeInForm}
+                        value={cpu_sample_work}/>
+              <WorkType name="Thread Sampling" attributes={["Frequency", "Max Frames"]} isDisabled={true}
+                        w_type="thread_sample" onChange={this.handleWorkTypeChangeInForm}
+                        value={thread_sample_work}/>
+              <WorkType name="Monitor Contention" attributes={["Max Monitors", "Max Frames"]} isDisabled={true}
+                        w_type="monitor_block" onChange={this.handleWorkTypeChangeInForm}
+                        value={monitor_block_work}/>
+              <WorkType name="Monitor Wait" attributes={["Max Monitors", "Max Frames"]} isDisabled={true}
+                        w_type="monitor_wait" onChange={this.handleWorkTypeChangeInForm}
+                        value={monitor_wait_work}/>
             </div>
           </div>
         </div>
--- ui/app/components/WorkTypeComponent/WorkTypeComponent.js
+++ ui/app/components/WorkTypeComponent/WorkTypeComponent.js
@@ -5,30 +5,39 @@ import React from 'react';
 import styles from './WorkTypeComponent.css';
 
 export default (props) => {
+  const first_attribute = props.attributes[0].toLowerCase().split(" ").join("_");
+  const second_attribute = props.attributes[1].toLowerCase().split(" ").join("_");
   return (
     <div
-      className={`mdl-cell mdl-shadow--2dp mdl-cell--3-col mdl-cell--4-col-tablet mdl-cell--4-col-phone ${props.isDisabled? styles.disabledDiv + " " + styles.comingSoon: ""}`}>
+      className={`mdl-cell mdl-shadow--2dp mdl-cell--3-col mdl-cell--4-col-tablet mdl-cell--4-col-phone ${props.isDisabled ? styles.disabledDiv + " " + styles.comingSoon : ""}`}>
       {/*<img src={coming_soon} className={}/>*/}
       <div className="mdl-card__title">
         <h2 className="mdl-card__title-text">{props.name}</h2>
+        {console.log(props)}
+        <span className="mdl-textfield__error">Input should be between 1-999!</span>
       </div>
       <div className="mdl-grid">
-        <div className="mdl-cell mdl-cell--12-col">
+        <div className="mdl-cell mdl-cell--12-col" >
           <div>{props.attributes[0]}</div>
           <div className="mdl-textfield mdl-js-textfield">
-            <input className="mdl-textfield__input" type="text" pattern="^[5-9][0-9]$|^100$"
-                   id={props.name + " " + props.attributes[0]}/>
+            <input className="mdl-textfield__input" type="number" min="50" max="100"
+                   data-w-type={props.w_type}  data-attribute={first_attribute}
+                   id={props.name + " " + props.attributes[0]}
+                   onChange={props.onChange} value={props.value[first_attribute]}/>
             <label className="mdl-textfield__label" htmlFor={props.name + " " + props.attributes[0]}>50</label>
-            <span className="mdl-textfield__error">Input should be between 50-100!</span>
+            <span className="mdl-textfield__error">Input should be between 50-100</span>
           </div>
         </div>
         <div className="mdl-cell mdl-cell--12-col">
           <div>{props.attributes[1]}</div>
           <div className="mdl-textfield mdl-js-textfield">
-            <input className="mdl-textfield__input" type="text"
-                   pattern="^([1-9]|[1-9][0-9]|[1-9][0-9][0-9])$" id={props.name + " " + props.attributes[1]}/>
+            <input className="mdl-textfield__input" type="number" min="1" max="999" name={props.w_type}
+                   data-w-type={props.w_type}  data-attribute={second_attribute}
+                   id={props.name + " " + props.attributes[1]} onChange={props.onChange}
+                   value={props.value[second_attribute]}
+            />
             <label className="mdl-textfield__label" htmlFor={props.name + " " + props.attributes[1]}>64</label>
-            <span className="mdl-textfield__error">Input should be between 1-999!</span>
+            <span className="mdl-textfield__error">Input should be between 1-999</span>
           </div>
         </div>
       </div>
